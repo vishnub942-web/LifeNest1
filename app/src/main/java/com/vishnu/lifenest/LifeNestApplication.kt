da63c4472af58a6b60b4875f64bdeeb99736a399
vishnub942-web <vishnub942@gmail.com>
2026-07-20 10:34:23 +0400

    Update LifeNestApplication.kt

--- app/src/main/java/com/vishnu/lifenest/LifeNestApplication.kt
+++ app/src/main/java/com/vishnu/lifenest/LifeNestApplication.kt
@@ -1,15 +1,22 @@
 package com.vishnu.lifenest
 
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
+import android.os.Bundle
 import android.os.Process
+import com.vishnu.lifenest.util.LockState
 import java.io.PrintWriter
 import java.io.StringWriter
 import kotlin.system.exitProcess
 
 class LifeNestApplication : Application() {
+
+    private var startedActivityCount = 0
+
     override fun onCreate() {
         super.onCreate()
+
         Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
             try {
                 val sw = StringWriter()
@@ -25,5 +32,26 @@ class LifeNestApplication : Application() {
             Process.killProcess(Process.myPid())
             exitProcess(1)
         }
+
+        // When every activity has stopped, the app is truly in the background --
+        // that's when we should require the PIN again next time it's opened.
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityStarted(activity: Activity) {
+                startedActivityCount++
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                startedActivityCount--
+                if (startedActivityCount <= 0) {
+                    LockState.unlocked = false
+                }
+            }
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
     }
 }
